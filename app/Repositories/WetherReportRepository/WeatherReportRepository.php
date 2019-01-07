f3c4008bb6c95532621808b274be899bceca2ebe
vovchello <vovka9379@gmail.com>
2019-01-07 10:44:32 +0000

    add method save

--- app/Repositories/WetherReportRepository/WeatherReportRepository.php
+++ app/Repositories/WetherReportRepository/WeatherReportRepository.php
@@ -9,18 +9,35 @@
 namespace App\Repositories\WetherReportRepository;
 
 
+use App\Models\WeatherReport\WeatherReport;
 use App\Repositories\WetherReportRepository\Contact\WeatherReportRepositoryInterface;
 
 class WeatherReportRepository implements WeatherReportRepositoryInterface
 {
+    private $weatherReport;
+
+    public function __construct(WeatherReport $weatherReport)
+    {
+        $this->weatherReport = $weatherReport;
+    }
+
     public function getAll()
     {
-        // TODO: Implement getAll() method.
+        return $this->weatherReport->getAttributes();
     }
 
     public function save($data): void
     {
-        // TODO: Implement save() method.
+        $this->weatherReport->setTemperature($data['temperature'];
+        $this->weatherReport->setMaxtemperature($data['max_temperature']);
+        $this->weatherReport->setMinTemperature($data['min_temperature']);
+        $this->weatherReport->setWeather($data['weather']);
+        $this->weatherReport->setWindSpeed($data['wind_speed']);
+        $this->weatherReport->setClouds($data['clouds']);
+        $this->weatherReport->setPrecipitationType($data['precipitation_type']);
+        $this->weatherReport->setPrecipitationSize($data['precipitation_size']);
+        $this->weatherReport->setData($data['data']);
+        $this->weatherReport->save();
     }
 
 }
