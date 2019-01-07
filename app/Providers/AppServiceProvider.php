d6136e96c8bb2085658771ccd1efaf37093fabe7
vovchello <vovka9379@gmail.com>
2019-01-07 10:28:04 +0000

    add repository and bind it at app/providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\WetherReportRepository\Contact\WeatherReportRepositoryInterface;
+use App\Repositories\WetherReportRepository\WeatherReportRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(WeatherReportRepositoryInterface::class, WeatherReportRepository::class);
     }
 }
---
+++ app/Repositories/WetherReportRepository/WeatherReportRepository.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panda
+ * Date: 07.01.19
+ * Time: 12:25
+ */
+
+namespace App\Repositories\WetherReportRepository;
+
+
+use App\Repositories\WetherReportRepository\Contact\WeatherReportRepositoryInterface;
+
+class WeatherReportRepository implements WeatherReportRepositoryInterface
+{
+    public function getAll()
+    {
+        // TODO: Implement getAll() method.
+    }
+
+    public function save($data): void
+    {
+        // TODO: Implement save() method.
+    }
+
+}
