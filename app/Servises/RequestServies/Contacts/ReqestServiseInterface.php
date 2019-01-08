ba404900a351a791876a5d0d6f827c87970395cd
vovchello <vovka9379@gmail.com>
2019-01-08 11:29:01 +0000

    create interface for requiest service

---
+++ app/Servises/RequestServies/Contacts/ReqestServiseInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panda
+ * Date: 08.01.19
+ * Time: 13:10
+ */
+
+namespace App\Servises\RequestServies\Contacts;
+
+
+use PhpParser\Builder\Interface_;
+
+interface ReqestServiseInterface
+{
+
+}
