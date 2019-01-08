357bc64398b15b267282564f74a207ccd8f2cae3
vovchello <vovka9379@gmail.com>
2019-01-08 11:56:22 +0000

    add weatherservice to the config

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,31 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use GuzzleHttp\Client;
+use App\Servises\RequestServies\RequestService;
+
 
 class HomeController extends Controller
 {
-    private $guzzle;
+    private $request;
 
     /**
      * HomeController constructor.
      * @param $guzzle
      */
-    public function __construct(Client $guzzle)
+    public function __construct(RequestService $request)
     {
-        $this->guzzle = $guzzle;
+        $this->request = $request;
     }
 
 
     public function index()
     {
-        $requiest = $this->guzzle->request('get','api.openweathermap.org/data/2.5/forecast',[
-            'query' =>[
-                'q' => 'Ottawa, ca',
-                'appid' => '02c10ef435d6119a32450932ac127016'
-            ]
-        ]);
-        $data = json_decode($requiest->getBody()->getContents());
-        dd($data->list);
+
+        $this->request->getRequiest();
+//        $requiest = $this->guzzle->request('get','api.openweathermap.org/data/2.5/forecast',[
+//            'query' =>[
+//                'q' => 'Ottawa, ca',
+//                'appid' => '02c10ef435d6119a32450932ac127016'
+//            ]
+//        ]);
+//        $data = json_decode($requiest->getBody()->getContents());
+//        dd($data->list);
     }
 }
--- app/Servises/RequestServies/Contacts/ReqestServiseInterface.php
+++ app/Servises/RequestServies/Contacts/ReqestServiseInterface.php
@@ -1,17 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: panda
- * Date: 08.01.19
- * Time: 13:10
- */
 
 namespace App\Servises\RequestServies\Contacts;
 
-
-use PhpParser\Builder\Interface_;
-
 interface ReqestServiseInterface
 {
-
+    public function getRequiest();
 }
