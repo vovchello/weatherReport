ea54655bf9ecea3ec0d8e6f310d6706501ba41fd
vovchello <vovka9379@gmail.com>
2019-01-21 22:06:00 +0000

    error is cleared

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
     public function index()
     {
         $data = $this->weatherService->getWeather('ca', 'Ottawa');
-
+        dd(config('weatherReport.city.list.json'));
         return view('base.home');
     }
 }
