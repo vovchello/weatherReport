8eec411aa0dfae29da07e4940bdc2e6d076d117c
vovchello <vovka9379@gmail.com>
2019-01-22 09:25:45 +0000

    add cities service

---
+++ app/Servises/CitiesService/CitiesService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panda
+ * Date: 22.01.19
+ * Time: 11:24
+ */
+
+namespace App\Servises\CitiesService;
+
+
+class CitiesService
+{
+
+}
---
+++ app/Servises/CitiesService/Contract/CitiesServiceInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panda
+ * Date: 22.01.19
+ * Time: 11:25
+ */
+
+namespace App\Servises\CitiesService\Contract;
+
+
+interface CitiesServiceInterface
+{
+
+}
