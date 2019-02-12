7e3dea84f547000065184e74de3501b9763c079a
vovchello <vovka9379@gmail.com>
2019-02-12 20:13:45 +0200

    reduced code from controllers and view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Servises\FindCitiesService\FindCitiesService;
-use App\Servises\JsonService\Contracts\JsonSserviceInterface;
-use App\Servises\ApiService\Contacts\WeatherServiceInterface;
-
-
 /**
  * Class HomeController
  * @package App\Http\Controllers
--- resources/views/base/home.blade.php
+++ resources/views/base/home.blade.php
@@ -56,10 +56,5 @@
         </script>
 
         <link rel="stylesheet" href="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.10.3/themes/flick/jquery-ui.css" />
-
-        <form action="" method="post" name="form_citydetails" id="form_citydetails" enctype="multipart/form-data" onsubmit="return false;">
-            <p><b>Please enter</b> your city here to see it work. <input class="ff_elem" type="text" name="ff_nm_from[]" value="" id="f_elem_city"/>
-        </form>
-
     </div>
 @endsection
